d3cb141fdf9957838cc2fb052c13ed95db91bf12
Jorge A. Flores-Morales <jorge.floresm900@gmail.com>
2017-01-28 13:09:55 -0600

    dev branch init commit 2: electric boogaloo

--- src/org/usfirst/frc/team4664/robot/Robot.java
+++ src/org/usfirst/frc/team4664/robot/Robot.java
@@ -1,62 +1,42 @@
 package org.usfirst.frc.team4664.robot;
 
-import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.RobotDrive;
 import edu.wpi.first.wpilibj.SampleRobot;
+import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.Timer;
-import edu.wpi.first.wpilibj.Victor; 
 
 public class Robot extends SampleRobot {
+	//Drive System Init
+	RobotDrive driveSystem = new RobotDrive(0, 1);
 	
-	public void R
-
-	/**
-	 * This autonomous (along with the chooser code above) shows how to select
-	 * between different autonomous modes using the dashboard. The sendable
-	 * chooser code works with the Java SmartDashboard. If you prefer the
-	 * LabVIEW Dashboard, remove all of the chooser code and uncomment the
-	 * getString line to get the auto name from the text box below the Gyro
-	 *
-	 * You can add additional auto modes by adding additional comparisons to the
-	 * switch structure below with additional strings. If using the
-	 * SendableChooser make sure to add them to the chooser code above as well.
-	 */
-	@Override
-	public void autonomousInit() {
-		autoSelected = chooser.getSelected();
-		// autoSelected = SmartDashboard.getString("Auto Selector",
-		// defaultAuto);
-		System.out.println("Auto selected: " + autoSelected);
+	//Joystick Declaration
+	Joystick gamepad = new Joystick(0);
+	Joystick stick   = new Joystick(1);
+	
+	//RobotStuff
+	public Robot() {
+		driveSystem.setExpiration(0.1);
 	}
 
-	/**
-	 * This function is called periodically during autonomous
-	 */
-	@Override
-	public void autonomousPeriodic() {
-		switch (autoSelected) {
-		case customAuto:
-			// Put custom auto code here
-			break;
-		case defaultAuto:
-		default:
-			// Put default auto code here
-			break;
+	//Init Stuff
+	public void robotInit() {
+	}
+
+	//Autonomous, activated in Autonomous mode
+	public void autonomous() {
+	}
+
+	//Operator mode, activated in TeleOperated mode
+	public void operatorControl() {
+		//TODO Test actual code
+		driveSystem.setSafetyEnabled(true);
+		while (isOperatorControl() && isEnabled()) {
+			driveSystem.arcadeDrive(gamepad);
+			Timer.delay(0.005);
 		}
 	}
 
-	/**
-	 * This function is called periodically during operator control
-	 */
-	@Override
-	public void teleopPeriodic() {
-	}
-
-	/**
-	 * This function is called periodically during test mode
-	 */
-	@Override
-	public void testPeriodic() {
+	//Test mode, activated in Test mode
+	public void test() {
 	}
 }
-
