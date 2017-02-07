230fbf7b8d6f22f637a1471619fb2eb4bbb98e7b
Droid_22 <Ethan.Honzik1@gmail.com>
2017-02-07 15:43:05 -0600

    .

--- src/org/usfirst/frc/team4664/robot/Robot.java
+++ src/org/usfirst/frc/team4664/robot/Robot.java
@@ -2,23 +2,23 @@ package org.usfirst.frc.team4664.robot;
 
 import edu.wpi.first.wpilibj.RobotDrive;
 import edu.wpi.first.wpilibj.SampleRobot;
-import edu.wpi.first.wpilibj.AnalogGyro;
+//import edu.wpi.first.wpilibj.AnalogGyro;
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.Timer;
 
 public class Robot extends SampleRobot {
-	public AnalogGyro gyro;	
+	//public AnalogGyro gyro;	
 	RobotDrive driveSystem = new RobotDrive(0, 1);
 	
-	Range_Finder ultraSonic;
+	//Range_Finder ultraSonic;
 	//Joystick Declaration
 	Joystick gamepad = new Joystick(0);
-	Joystick stick   = new Joystick(1);
-	Robot() {
+	//Joystick stick   = new Joystick(1);
+	public Robot() {
 		driveSystem.setExpiration(0.1);
-		gyro = new AnalogGyro(0);
-		gyro.calibrate();
-		ultraSonic=new Range_Finder(3);
+		//gyro = new AnalogGyro(0);
+		//gyro.calibrate();
+		//ultraSonic=new Range_Finder(3);
 	}
 
 	//Init Stuff
@@ -51,13 +51,13 @@ public class Robot extends SampleRobot {
 	}
 	public void auto0(){
 		while(isEnabled()){
-			if(ultraSonic.getDistance()>30){
-				driveSystem.arcadeDrive(0.5, gyro.getAngle()/18);
+			//if(ultraSonic.getDistance()>30){
+				//driveSystem.arcadeDrive(0.5, gyro.getAngle()/18);
 				//Limit reached Driving blind for 1 seconds
-			}else{
-				Timer.delay(1);
-				driveSystem.arcadeDrive(0, 0);
-			}
+			//}else{
+			//	Timer.delay(1);
+			//	driveSystem.arcadeDrive(0, 0);
+			//}
 		}
 	}
 }
